7711b5cee7128394c32ab3c815291a60438e2070
Oliver Oppitz <ooppitz@gmail.com>
2021-09-22 13:47:09 +0200

    Add code for showing reg expresseions

--- src/main/java/de/die_gfi/oliver/playground/StringGames.java
+++ src/main/java/de/die_gfi/oliver/playground/StringGames.java
@@ -8,14 +8,22 @@ public class StringGames {
 
     public static void main(String[] args) {
 
+    String input = "fjslkfjf99jkljlk8454jlkjljl545ljkj5454jljljj555";
+            // "Peter, Franz,             Verena,Sophia";
+    String[] words = input.split("\\d+");
+    System.out.println("'" + input + "'" + " --> " + Arrays.toString(words));
 
+
+
+    }
+
+    private static void extracted1() {
         String[]  array1 =  SortInteractive.extractNames("\t\t");
         if ( array1 != null) {
             System.out.println("length=" + array1.length);
             System.out.println(Arrays.toString(array1));
             System.out.println("'" + array1[0] + "'");
         }
-
     }
 
     private static void extracted() {
