5a839280067d06e6d43c4c1ecde11e98c5c16328
Oliver Oppitz <ooppitz@gmail.com>
2021-09-25 21:36:03 +0200

    Cosmetics

--- src/test/java/de/die_gfi/oliver/kw39/foodorder/OrderTest.java
+++ src/test/java/de/die_gfi/oliver/kw39/foodorder/OrderTest.java
@@ -18,13 +18,11 @@ class OrderTest {
                 Assertions.assertEquals(0, p2.toPay);
                 Assertions.assertEquals(0, p3.toPay);
 
-
                 w.orderMeal(p1, 1,0,2,1,2);
                 w.orderMeal(p2, 1,0,1,1,1);
                 w.orderMeal(p3, 2,2,2,2,0);
 
-                // check if the orders are counted correctly
-
+                // Number of orders per item:
                 // Mon: A-2 B-1
                 // Tue: A-0 B-1
                 // Wed: A-1 B-2
