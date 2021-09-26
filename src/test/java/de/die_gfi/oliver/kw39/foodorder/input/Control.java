0ee565342f1cef1934c3cb177468b2129dcb1035
Oliver Oppitz <ooppitz@gmail.com>
2021-09-26 15:48:05 +0200

    Implement entering data for a person

--- src/test/java/de/die_gfi/oliver/kw39/foodorder/input/Control.java
+++ src/test/java/de/die_gfi/oliver/kw39/foodorder/input/Control.java
@@ -2,18 +2,40 @@ package de.die_gfi.oliver.kw39.foodorder.input;
 
 // TODO Implement the control (text-based) user interface
 
+import de.die_gfi.oliver.kw39.foodorder.logic.Person;
+
+import java.util.Scanner;
+
 public class Control {
 
-    // The control needs to be able to invoke (all?) the public methods from the logic package
 
-    // Functions for the person
-    // - Create / select a person
-    // - Order meals
-    // - Print individual report
+    public static void main(String[] args) {
 
-    // Functions for the kitchen
-    // - print report
-    // - change menu (phase 2)
+        // The control needs to be able to invoke (all?) the public methods from the logic package
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Functions for the person
+        // - Create / select a person
+
+        System.out.println("Enter first name(s):");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name(s):");
+        String lastName = scanner.nextLine();
+
+        Person p = new Person(firstName,lastName);
+
+
+
+        System.out.println("The current menu is");
+
+
+        // - Print individual report
+
+        // Functions for the kitchen
+        // - print report
+        // - change menu (phase 2)
+    }
 
 
 }
