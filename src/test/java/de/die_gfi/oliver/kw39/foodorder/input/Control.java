d138209a6254e2193aca725b06dd2da0bed6cd7f
Oliver Oppitz <ooppitz@gmail.com>
2021-09-26 11:37:30 +0200

    Add comments for Control class

--- src/test/java/de/die_gfi/oliver/kw39/foodorder/input/Control.java
+++ src/test/java/de/die_gfi/oliver/kw39/foodorder/input/Control.java
@@ -3,4 +3,17 @@ package de.die_gfi.oliver.kw39.foodorder.input;
 // TODO Implement the control (text-based) user interface
 
 public class Control {
+
+    // The control needs to be able to invoke (all?) the public methods from the logic package
+
+    // Functions for the person
+    // - Create / select a person
+    // - Order meals
+    // - Print individual report
+
+    // Functions for the kitchen
+    // - print report
+    // - change menu (phase 2)
+
+
 }
